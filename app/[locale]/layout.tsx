aa018cffaa2a8a2ec380b685941c5f89d2dbaac4
Kao Bui <kaonghia035@gmail.com>
2026-02-23 12:48:51 +0100

    Add insight and analytics

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,41 +1,44 @@
-import type {Metadata} from "next";
-import {Arimo} from "next/font/google";
-import {NextIntlClientProvider, hasLocale} from "next-intl";
-import {getMessages, setRequestLocale} from "next-intl/server";
-import {notFound} from "next/navigation";
+import type { Metadata } from "next";
+import { Arimo } from "next/font/google";
+import { NextIntlClientProvider, hasLocale } from "next-intl";
+import { getMessages, setRequestLocale } from "next-intl/server";
+import { notFound } from "next/navigation";
 
 import LenisProvider from "@/app/providers/lenis-providers";
-import {routing} from "@/i18n/routing";
+import { routing } from "@/i18n/routing";
 
 import "../globals.css";
 import Footer from "../Footer";
 import Header from "../Header";
 import IntroOverlay from "../components/IntroOverlay";
 
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 const arimo = Arimo({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
   style: ["normal", "italic"],
   variable: "--font-arimo",
-  display: "swap"
+  display: "swap",
 });
 
 export const metadata: Metadata = {
   title: "YEO VIETNAM",
-  description: "Tổ chức Định hướng và Phát triển Tiềm năng trẻ Việt Nam"
+  description: "Tổ chức Định hướng và Phát triển Tiềm năng trẻ Việt Nam",
 };
 
 export function generateStaticParams() {
-  return routing.locales.map((locale) => ({locale}));
+  return routing.locales.map((locale) => ({ locale }));
 }
 
 type Props = {
   children: React.ReactNode;
-  params: Promise<{locale: string}>;
+  params: Promise<{ locale: string }>;
 };
 
-export default async function LocaleLayout({children, params}: Props) {
-  const {locale} = await params;
+export default async function LocaleLayout({ children, params }: Props) {
+  const { locale } = await params;
 
   if (!hasLocale(routing.locales, locale)) {
     notFound();
@@ -55,6 +58,8 @@ export default async function LocaleLayout({children, params}: Props) {
             <Footer />
           </LenisProvider>
         </NextIntlClientProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
